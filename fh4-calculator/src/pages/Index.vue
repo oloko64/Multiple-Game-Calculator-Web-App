186c32ae996adbefb8265bffc57659f45050fd82
OLoko64 <naldoepingo1@gmail.com>
2021-05-06 16:02:24 -0300

    Added home

--- fh4-calculator/src/pages/Index.vue
+++ fh4-calculator/src/pages/Index.vue
@@ -1,15 +1,21 @@
 <template>
-  <q-page class="q-pa-lg row justify-center full-height full-width text-center">
+  <q-page class="q-pa-lg text-center">
+    <div class="row justify-center">
+      <div class="text-h5 col-12 col-md-4 bg-primary rounded-borders q-pa-md q-ma-sm">
+        Forza Horizon 4 Tunning Calculator
+      </div>
+      <div class="text-h5 col-12 col-md-4 bg-custom rounded-borders q-pa-md q-ma-sm">
+        Arma 3 Fov Calculator
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
-  methods: {
-  }
+  methods: {}
 };
 </script>
